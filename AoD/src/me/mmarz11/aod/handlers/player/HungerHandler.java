451128cecc5968f3e76211cf0b1cfc887ff4af24
Mmarz11 <Mmarz11@inbox.com>
2014-01-18 22:15:04 -0500

    Added no hunger loss for hunters

--- AoD/src/me/mmarz11/aod/handlers/player/HungerHandler.java
+++ AoD/src/me/mmarz11/aod/handlers/player/HungerHandler.java
@@ -4,23 +4,36 @@ import me.mmarz11.aod.AoD;
 import me.mmarz11.aod.enums.Timer;
 import me.mmarz11.aod.handlers.round.TimerHandler;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class HungerHandler implements Listener {
 	private AoD plugin;
-	
+
 	public HungerHandler(AoD plugin) {
 		this.plugin = plugin;
-		this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
+		this.plugin.getServer().getPluginManager()
+				.registerEvents(this, this.plugin);
 	}
-	
+
 	@EventHandler
 	public void onFoodLevelChangeEvent(FoodLevelChangeEvent event) {
+		Entity entity = event.getEntity();
+
 		TimerHandler handler = this.plugin.handlers.timerHandler;
 		if (handler.getCurrentTimer() != Timer.ROUND) {
 			event.setCancelled(true);
+		} else {
+			if (entity instanceof Player) {
+				Player player = (Player) entity;
+				if (this.plugin.handlers.playerTypeHandler.hunters
+						.contains(player.getName())) {
+					event.setCancelled(true);
+				}
+			}
 		}
 	}
 }
